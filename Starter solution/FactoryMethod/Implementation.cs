963df09688d7730bb801deb5a82828cefefa05d0
Chris Crawford <christopher.crawford@insperity.com>
2022-12-21 09:01:22 -0600

    factory method

--- Starter solution/FactoryMethod/Implementation.cs
+++ Starter solution/FactoryMethod/Implementation.cs
@@ -1,4 +1,62 @@
 ﻿namespace FactoryMethod
 {
-   
+    public abstract class DiscountService
+    {
+        public abstract int DiscountPercentage { get; }
+        public override string ToString() => GetType().Name;
+    }
+
+    public class CountryDiscountService : DiscountService
+    {
+        private readonly string _countryIdentifier;
+
+        public CountryDiscountService(string countryIdentifier)
+        {
+            _countryIdentifier = countryIdentifier;
+        }
+
+        public override int DiscountPercentage
+        {
+            get
+            {
+                return _countryIdentifier switch
+                {
+                    "BE" => 20,
+                    _ => 10
+                };
+            }
+        }
+    }
+
+    public class CodeDiscountService : DiscountService
+    {
+        private readonly Guid _code;
+
+        public CodeDiscountService(Guid code)
+        {
+            _code = code;
+        }
+
+        public override int DiscountPercentage => 15;
+    }
+
+    public abstract class DiscountFactory
+    {
+        public abstract DiscountService CreateDiscountService();
+    }
+
+    public class CountryDiscountFactory : DiscountFactory
+    {
+        private readonly string _countryIdentifier;
+
+        public CountryDiscountFactory(string countryIdentifier)
+        {
+            _countryIdentifier = countryIdentifier;
+        }
+
+        public override DiscountService CreateDiscountService()
+        {
+            return new CountryDiscountService(_countryIdentifier);
+        }
+    }
 }
