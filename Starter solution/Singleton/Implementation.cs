87f2263b55180bab9f7c81c5a64b8aef3abcc032
Chris Crawford <chris@vcnws.com>
2022-12-20 19:06:46 -0600

    singleton

--- Starter solution/Singleton/Implementation.cs
+++ Starter solution/Singleton/Implementation.cs
@@ -1,4 +1,16 @@
 ﻿namespace Singleton
 {
-  
+    public class Logger
+    {
+        private static readonly Lazy<Logger> _lazyLogger = new Lazy<Logger>(() => new Logger());
+
+        protected Logger(){}
+
+        public static Logger Instance => _lazyLogger.Value;
+
+        public void Log(string message)
+        {
+            Console.WriteLine($"Message: {message}");
+        }
+    }
 }
